d5a2e1faebc2b29d9bc65ae306b5a9c3cc248863
jerryboss322 <jerryadewole2023@gmail.com>
2026-06-24 00:01:57 +0100

    Fix Vercel build

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,1 +1,14 @@
-<?php return require __DIR__.'/../vendor/laravel/framework/src/Illuminate/Foundation/Application.php'::configure(basePath: dirname(__DIR__))->create();
+<?php
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {})
+    ->withExceptions(function (Exceptions $exceptions) {})
+    ->create();
