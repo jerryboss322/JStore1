51bbbb0ec7b7ae56f2b425bd167d1890613dd813
jerryboss322 <jerryadewole2023@gmail.com>
2026-06-23 23:57:23 +0100

    Add Vercel PHP runtime

---
+++ api/index.php
@@ -0,0 +1,6 @@
+<?php define('LARAVEL_START', microtime(true));
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
+$app->make(Illuminate\Contracts\Http\Kernel::class)
+    ->handle(Illuminate\Http\Request::capture())
+    ->send();
