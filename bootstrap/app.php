a725efa6106d56b1e8704020046ff10795d6ad3a
jerryboss322 <jerryadewole2023@gmail.com>
2026-06-23 23:55:39 +0100

    Fix structure for Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,1 @@
-<?php
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(web: __DIR__.'/../routes/web.php', commands: __DIR__.'/../routes/console.php', health: '/up')
-    ->withMiddleware(function (Middleware $middleware) {})
-    ->withExceptions(function (Exceptions $exceptions) {})->create();
+<?php return require __DIR__.'/../vendor/laravel/framework/src/Illuminate/Foundation/Application.php'::configure(basePath: dirname(__DIR__))->create();
--- public/index.php
+++ public/index.php
@@ -1,12 +1,6 @@
-<?php
-use Illuminate\Http\Request;
-define('LARAVEL_START', microtime(true));
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+<?php define('LARAVEL_START', microtime(true));
 require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle($request = Request::capture());
-$response->send();
-$kernel->terminate($request, $response);
+$app->make(Illuminate\Contracts\Http\Kernel::class)
+    ->handle(Illuminate\Http\Request::capture())
+    ->send();
